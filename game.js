1d9f99cd7e4551a48a31b7ba67de60c7cd3af776
youngengineer1234 <theyoungengineer.com@gmail.com>
2021-12-27 12:36:06 +0530

    second commit

--- game.js
+++ game.js
@@ -1,31 +1,77 @@
-userClickedPattern =[];
 bottonColours = ["red", "green", "blue", "yellow"];
 
 gamePattern = [];
+userClickedPattern =[];
+
+var started = false;
+
+var level = 0;
+
+$(window).on("keypress", function(event) {
+
+    if(!started) {
+        $("#level-title").text("level " + level);
+        nextSequence();
+        started = true;
+    }
+
+});
+
+$(".btn").on("click", function() {
+    var userChosenColor = $(this).attr("id")
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer((userClickedPattern.length)-1);
+
+});
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        // console.log("Wrong");
+        $(document).addClass("game-over");
+
+        setTimeout(function() {
+            $(document).removeClass("game-over");
+        }, 2000);
+
+        var wrong_2 = new Audio("sounds/wrong.mp3")
+        wrong_2.play();
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        started = false;
+        level = 0;
+        userClickedPattern = [];
+        gamePattern = [];
+    }
+}
 
 function nextSequence() {
+
+    userClickedPattern = [];
 
     var randomNumber = Math.round(Math.random()*3);
     var randomChosenColours = bottonColours[randomNumber];
     gamePattern.push(randomChosenColours);
 
     playSound(randomChosenColours);
+    level += 1;
+    $("#level-title").text("level " + level);
+
 
 }
-
-setTimeout(function animatePress(currentColour) {
-    $("#"+currentColour).addClass("pressed")
-},100);
-
-$(".btn").on("click", function() {
-    var userChosenColor = $(this).attr("id")
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern)
-
-    playSound(userChosenColor);
-    animatePress(userChosenColor)
-
-})
 
 function playSound(name) {
 
@@ -35,4 +81,15 @@ function playSound(name) {
     audio.play();
 
 }
+
+function animatePress(currentColor) {
+    $("#"+currentColor).addClass("pressed");
+
+    setTimeout(function() {
+        $("#"+currentColor).removeClass("pressed");
+    }, 100);
+}
+
+
+
 
